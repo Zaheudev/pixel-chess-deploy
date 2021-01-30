0d9693505a4ff35495400ba0d99114c76f62acf9
Tudor Velican <tudor@velican.org>
2021-01-30 15:32:06 +0100

    Configured default routes and EJS views

--- pixel-chess/app.js
+++ pixel-chess/app.js
@@ -1,9 +1,38 @@
-var express = require("express");
-var http = require("http");
+const express = require("express");
+const http = require("http");
+const indexRouter = require("./routes/index");
+const path = require("path");
 
-var port = process.argv[2];
-var app = express();
+const port = process.argv[2];
+const app = express();
 
 app.use(express.static(__dirname + "/public"));
+app.set("views",path.join(__dirname, 'views'));
+app.set('view engine','ejs');
+
+app.get("/", indexRouter);
+app.get("/play", indexRouter);
+/*
+app.get('/', function(req,res) {
+  const title = "Pixel Chess";
+  const viewData = {
+    title: title
+  }
+  res.render('index', viewData);
+})
+*/
+
+//TODO: Error code pages using EJS templating
+/*
+app.use(function(req,res) {
+  res.status(404).send("404: Page not Found!");
+})
+app.use(function(req,res) {
+  res.status(500).send("500: Internal Server Error!")
+})
+*/
+
+
+
 http.createServer(app).listen(port);
 console.log("Server started on port " + port);
--- pixel-chess/routes/index.js
+++ pixel-chess/routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var app = express();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+/* When pressing Play button, return this page */
+router.get('/play', function(req,res) {
+  res.sendFile("game.html", {root: "./public"})
+});
+
+/* GET splash screen home page. */
+router.get('/', function(req, res) {
+  res.sendFile("splash.html", {root: "./public"})
 });
 
 module.exports = router;
