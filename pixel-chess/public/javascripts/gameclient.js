3bfacfadaf46cbbb16c3c6e235b78fbe9b39d66c
Zaheudev <68231115+Zaheudev@users.noreply.github.com>
2021-02-04 01:02:30 +0200

    now when you click a cell and it s an image attached, it returns the coordonates and the image element else returns just the coordonates. The "Waiting for opponent..." text is updating when the game starts.

--- pixel-chess/public/javascripts/gameclient.js
+++ pixel-chess/public/javascripts/gameclient.js
@@ -1,17 +1,25 @@
 const client = new WebSocket("ws://localhost:8080");
 
 var board = document.querySelector(".chess-board");
+var state = document.querySelector("#state");
 
 client.onmessage = function (event) {
     console.log(event.data);
+    if(event.data === "Started"){
+        state.innerHTML = "Starting...";
+    }
 }
-
-console.log(board);
 
 board.addEventListener('click', (e)=>{
     if (e.target.nodeName.toUpperCase() === 'TD') {
+        if(document.querySelector("#"+e.target.id).hasChildNodes()){
+            let imgCell = document.querySelector("#"+e.target.id);
+            console.log(imgCell.firstElementChild);
+        }
        console.log(e.target.id);
     }else if(e.target.nodeName.toUpperCase() === 'IMG'){
+        let cellImg = document.querySelector("#"+e.target.id);
+        console.log(cellImg.parentElement.id);
         console.log(e.target);
     }
 });
