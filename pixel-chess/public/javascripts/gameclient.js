87168be8ea9900dcb757aa08c8d5a86677c8c9d3
Zaheudev <68231115+Zaheudev@users.noreply.github.com>
2021-02-03 21:58:59 +0200

    added pieces to chess-board and when client clicks on a cell his id or image (image is temporary)

--- pixel-chess/public/javascripts/gameclient.js
+++ pixel-chess/public/javascripts/gameclient.js
@@ -1,5 +1,17 @@
 const client = new WebSocket("ws://localhost:8080");
 
+var board = document.querySelector(".chess-board");
+
 client.onmessage = function (event) {
     console.log(event.data);
 }
+
+console.log(board);
+
+board.addEventListener('click', (e)=>{
+    if (e.target.nodeName.toUpperCase() === 'TD') {
+       console.log(e.target.id);
+    }else if(e.target.nodeName.toUpperCase() === 'IMG'){
+        console.log(e.target);
+    }
+});
